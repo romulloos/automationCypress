231c923977af18004bd5e9a6d6c2f923dcc9b07c
romulloos <romullo.os@gmail.com>
2024-09-03 18:08:30 -0300

    testes de API

--- cypress/e2e/api/spec.cy.js
+++ cypress/e2e/api/spec.cy.js
@@ -1,5 +1,15 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    let name="HP%20H2310%20In-ear%20Headset"
+    let quantityPerEachCategory=-1;
+    cy.request({
+      method: 'GET',
+      url: `https://www.advantageonlineshopping.com/catalog/api/v1/products/search?name=${name}&quantityPerEachCategory=${quantityPerEachCategory}`,
+      headers: {
+        accept: "*/*",
+      }, 
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    })
   })
 })
