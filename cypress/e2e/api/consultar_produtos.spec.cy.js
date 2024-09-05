623aab97d097fd548fb59fb43e1d9b0ff498bf1a
romulloos <romullo.os@gmail.com>
2024-09-05 17:37:46 -0300

    inclusao dos testes

---
+++ cypress/e2e/api/consultar_produtos.spec.cy.js
@@ -0,0 +1,27 @@
+describe("Consulta de produtos", () => {
+  it("Validar consulta do produto HeadPhone H2310", () => {
+    let name = "H2310"
+    let quantityPerEachCategory = -1;
+    cy.request({
+      method: 'GET',
+      url: `https://www.advantageonlineshopping.com/catalog/api/v1/products/search?name=${name}&quantityPerEachCategory=${quantityPerEachCategory}`,
+      headers: {
+        accept: "*/*",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.categoryId).to.be.equal(2);
+      expect(response.body.categoryName).to.be.contains("HEADPHONES");
+      expect(response.body.categoryImageId).to.be.contains("headphones");
+      response.body.products.forEach(products => {
+        if (products.productId === 12) {
+          expect(products.productId).to.be.equal(12);
+          expect(products.categoryId).to.be.equal(2);
+          expect(products.productName).to.be.contains("HP H2310 In-ear Headset");
+          expect(products.price).to.be.equal(13.99);
+          expect(products.imageUrl).to.be.contains("2100");
+        }
+      });
+    });
+  });
+});
